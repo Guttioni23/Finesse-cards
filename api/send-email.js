78a7a7d3e80abeb04c9e499e77a44b35b107f118
Guttioni23 <humzahng@gmail.com>
2026-02-19 21:49:40 +1000

    update

--- api/send-email.js
+++ api/send-email.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
         'Authorization': `Bearer ${apiKey}`,
       },
       body: JSON.stringify({
-        from: 'Weekly Scorer <onboarding@resend.dev>',
+        from: 'Weekly Scorer <scorecards@finessecards.com>',
         to,
         subject,
         text,
